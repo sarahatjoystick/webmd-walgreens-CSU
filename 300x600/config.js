3530dd86cb1e503dbc26f3b48208d50abceb9a8f
jamorajj-jsi <jason@joystickinteractive.com>
2015-08-08 07:02:05 +0800

    adding files and readme

---
+++ 300x600/config.js
@@ -0,0 +1,58 @@
+var config = {
+	intro: "i_intro.jpg",
+ 	header: {
+ 		image: "i_300x101_header.jpg",
+ 		exit: "http://www.walgreens.com/"
+ 	},
+ 	footer: {
+ 		image: "i_300x100_footer.jpg",
+ 		exit: "http://www.walgreens.com/"
+ 	},
+ 	module : {
+ 		//close: "i_tab_close.png",
+ 		bg: "i_tab_bg.jpg",
+ 		watchMore: "watch_more.png",
+ 		listOn: "btn-list-on.png",
+ 		listOff: "btn-list-off.png",
+ 		mapOn: "btn-map-on.png",
+ 		mapOff: "btn-map-off.png",
+ 		close_color: "#858585",
+ 		close_color_locator: "#fafafa",
+ 		locator: {
+ 			exit: "http://www.walgreens.com/",
+ 			text: "Enter ZIP code below.",
+ 			button: "i_btn_zip.png"
+ 		},
+		gallery: {
+			poster: "i_290x100_gallery_smallposter.jpg",
+			photos: [
+				[
+					"i_img1.jpg", 
+					"Prevent your back pain",
+					"Simple floor exercises can help keep your arthritis pain in check."
+				],
+				[
+					"i_img2.jpg",
+					"Ease your aching knees",
+					"Work the muscles around your knees to support the joint and help you move easier."
+				],
+				[
+					"i_img3.jpg",
+					"Help your sore hips",
+					"Be sure to keep your balance with a sturdy table or railing when you try these hip-strengthening exercises."
+				]
+			]
+		},
+		video: {
+			title: "For What Matters Most",
+			description: "With two layers of medicine, TYLENOL&reg; Arthritis Relief soothes your aching joints so you can get back to your day. ",
+			poster: {
+				big: "video_300x168.png",
+				small: "i_290x100_movie_smallposter.png"
+			},
+			files: [
+				"http://www.webmdmobilevideo.com.edgesuite.net/media/cons_video_assets/sponsor/2015/tylenol/TYL-TARS-DEMO-063015.mp4"
+			]
+		}
+ 	}
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+WebMD | Walgreens Tylenol
+
+Basecamp: https://basecamp.com/2312286/projects/9787753
